baa79ef5955e2de16ff26b7424c877e3e31cf17f
alecasanchez <alecsanchez162@gmail.com>
2023-03-28 21:04:45 -0500

    coding with Dane

---
+++ CC_2023-15-03/app.js
@@ -0,0 +1,15 @@
+//function addFive(num) {
+//  var total = num + 5
+//  return num
+//}
+
+//P any special characters, any edge cases
+//R i have a number that is in the function parameter and needs to have 5 added to it. return new number
+//E rock band posters, there are five ants for now and the sixth ant becomes one of the twins of the local and colony. there is a grocery store that stocks items and the stocker has to move certain items up levels depending on price. there must be 5 items on the top shelf for every brand
+//P  I have to define num in the parameter of the function addFive
+
+function addFive(num){
+  var total = num + 5;
+  return total;
+}
+console.log(addFive(2));
