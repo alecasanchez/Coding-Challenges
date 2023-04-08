e0645cf17970fc6c95abcf2e8af423621b64890c
alecasanchez <alecsanchez162@gmail.com>
2023-04-08 15:03:37 -0500

    updated

---
+++ CC 2023-08-04/app.js
@@ -0,0 +1,20 @@
+//reverse order of a list
+function reverseList(list) {
+  reverse.list()
+}
+//P parameters/ obstacles?? special objects etc... none to discuss
+
+//R reverse the order of the lists 
+
+// nearly a palindrome program. uses numbers instead. Sam Franklin code concept to decode DNA Sequences. 
+
+//Function reverse list. 
+
+
+//What method reverses strings again???
+//produce a new list so maybe sort???
+//return function
+
+//Why is the argument list not defined still that it is in the local code body? 
+
+//Lists must be in brackets and separated by commas remember and understand that a list must be in brackets and separated by commas
