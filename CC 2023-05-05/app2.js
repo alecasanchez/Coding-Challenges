015eb21970e6ba3b87f047359ced450ad7592a0a
alecasanchez <alecsanchez162@gmail.com>
2023-05-06 12:33:10 -0500

    challenge

---
+++ CC 2023-05-05/app2.js
@@ -0,0 +1,7 @@
+function testEven(n) {
+  return n % 2 == 0; 
+}
+
+// function checks to see if number passed is even. function returns true or false. number may be positive, negative INTEGERS or FLOATS. floats with non even decimals are considered un even. 
+// pass a number function checks if true or false if even. integers /decomals are also checked and if passed return true. 
+
