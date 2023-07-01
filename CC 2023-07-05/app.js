7b87a65dbf781f4cbed28125074c486a3428b73b
alecasanchez <alecsanchez162@gmail.com>
2023-07-01 13:20:15 -0500

    update code challenge

---
+++ CC 2023-07-05/app.js
@@ -0,0 +1,11 @@
+//hex number to decimal number
+function hexToDec(hexString){
+  return  parseInt(hexString, 16);
+ }
+
+//edge cases none
+//hex decimal to string possibly use toString()? if not then reduce.
+//previous code challenge. the computer prints receipt, barista says it. self teaching math computer.
+
+//where does this 16 come from?? There are base 16 digits
+//What does the comma do? 
---
+++ CC 2023-10-05/app.js
@@ -0,0 +1,5 @@
+//Create a function that multiplies a number by 50 and then add 6 to the original number. if the value is a string, result is error. 
+
+function problem(x){
+  return (x * 50)+6;
+}
