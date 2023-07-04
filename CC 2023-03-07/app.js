f1df4b5957c1e79c2906f1f5fd9ed3e0cd6f1aa6
alecasanchez <alecsanchez162@gmail.com>
2023-07-04 15:31:38 -0500

    oops

---
+++ CC 2023-03-07/app.js
@@ -0,0 +1,17 @@
+//Parameters are null
+// let 1 equal the ground floor, the zeroth floor equal true zero and 15 equal 13
+function getRealFloor(n) {
+  // if n is greater than 13, so if this insatnstiates the cause or the need to operate this program
+  return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+}
+
+
+n greater than 13 
+
+n - 2 if true
+otherwise
+
+use this ternerary operator
+
+// how many years did the inventors live all together
+
