426896300ee79e47315baed635c843db3a437ef2
alecasanchez <alecsanchez162@gmail.com>
2023-07-17 15:08:28 -0500

    commit out

--- CC 2023-17-07/app.js
+++ CC 2023-17-07/app.js
@@ -3,23 +3,23 @@
 //I need to make sure I have this absolutely sure. i have a company with many floors with each level containing the same number of doors. on level one there is one door on level two there is two doors etc.. and the integer is the number of workers. we count how many times/ 5 workers times level 13 is 65. we only go up to ten correct? 
 
 //here we go. we need to use an array. and object. and a for loop
-for (let number = 0; number <= 10; number = number + 5) + '* $`{number}` = '{
-  console.log(number);
-}
-//" am I on the right track???"
-
-//create a function that uses a for loop to increment numbers until 10. 
-function inTimesTable(s){
-for (let number = 1; number < 10; number *= s++);
-}
-
-console.log('4')
-//const multiTable = (number) => {
-  let table = '';
-  
-  for(let i = 1; i <= 10; i++) {
-    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
-  }
-
-  return table;
-}
+//for (let number = 0; number <= 10; number = number + 5) + '* $`{number}` = '{
+//  console.log(number);
+//}
+////" am I on the right track???"
+//
+////create a function that uses a for loop to increment numbers until 10. 
+//function inTimesTable(s){
+//for (let number = 1; number < 10; number *= s++);
+//}
+//
+//console.log('4')
+////const multiTable = (number) => {
+//  let table = '';
+//  
+//  for(let i = 1; i <= 10; i++) {
+//    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+//  }
+//
+//  return table;
+//}
