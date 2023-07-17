161072302d33d7cdaa211f4a6c6ae18180165e65
alecasanchez <alecsanchez162@gmail.com>
2023-07-17 13:45:49 -0500

    correct

---
+++ CC 2023-05-07/app.js
@@ -0,0 +1,6 @@
+//Parameters are none
+//REword find the derivarive of the parameters remember the calculatorfrom eloquent js
+
+function derive(coefficient, exponent){
+  return (coefficient * exponent) + "x^" + (exponent - 1)
+};
