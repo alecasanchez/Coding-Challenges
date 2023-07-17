76abbcd63d5ea98b13fbae69be2cfa9cb95aeec8
alecasanchez <alecsanchez162@gmail.com>
2023-07-17 16:43:14 -0500

    attempted

---
+++ CC 2023-17-07/02app.js
@@ -0,0 +1,22 @@
+//the parameter in this problem is queue and queue is an array. what is queue? this will be the pseudo code
+//when the wolf is nearby as in the first in the array, say " get away from my sheep" when it is a sheep in the array, say something else
+//filing cabinets will have folders. we dont know how many folders each file may have. it will contain x number of folders. you are looking for a specific name of the folder. when you open a filing cabinet and its not the folder, "next folder" and when it is your folder say " hey thats my folder!"
+// create an array containing wolf and sheep 
+
+//const warnTheSheep = (queue) => { 
+//// warn the sheep will be the function 
+//
+////setting the array 
+//  let array = ["wolf", "sheep"]; 
+////however many wolves and sheep
+//  for(let i = number; i <= number; i++){ 
+//    if( legth - 1 = "wolf")
+//        console.log("hey get tf away from my sheep");
+//    }else{ 
+//    console.log("there is a wolf nearby")
+//  };
+//  return warnTheSheep;
+//};
+//   
+  
+  
