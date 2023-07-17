17f31b7ecda1ec9fa6a27f9482b1430aff5827b7
alecasanchez <alecsanchez162@gmail.com>
2023-07-17 13:46:05 -0500

    correct

---
+++ CC 2023-08-07/app.js
@@ -0,0 +1,11 @@
+//Parameters a is the array 
+//Reword use splice method to search for multiple instances in an array. if there are multiple instances in an array, take them out and then return the new array.
+//Edge cases none
+//Pseudo code
+
+function distinct() {
+  const array = []
+  array.splice(i++){
+    return []; 
+  } 
+}
