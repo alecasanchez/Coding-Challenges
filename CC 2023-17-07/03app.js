991c151357d569c3e45889c97dc6a4cc0a2b13a8
alecasanchez <alecsanchez162@gmail.com>
2023-07-17 21:21:53 -0500

    anotha one

---
+++ CC 2023-17-07/03app.js
@@ -0,0 +1,3 @@
+//n is the parameter of digitize, the function. You must first instantiate an array containing the numbers. and then produce the numbers back in reverse order. 
+//any number that is non negative needs to have their digits separated. and put into an array. Splice??? Split???
+//similar to the palindrome problem index of???
