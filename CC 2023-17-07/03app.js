250bf5f014d87688d5338feadac0a96c9db87998
alecasanchez <alecsanchez162@gmail.com>
2023-07-18 15:55:03 -0500

    first commit

--- CC 2023-17-07/03app.js
+++ CC 2023-17-07/03app.js
@@ -1,3 +1,11 @@
 //n is the parameter of digitize, the function. You must first instantiate an array containing the numbers. and then produce the numbers back in reverse order. 
 //any number that is non negative needs to have their digits separated. and put into an array. Splice??? Split???
 //similar to the palindrome problem index of???
+// create a function
+
+
+//function digitize(n) {
+//  let array = [];
+//  for(let i = 1, i <= array.length, i++);
+////this means that let the index equal 1 and for until the index reaches the array length increase in increments of i. what I need is to 
+//}
