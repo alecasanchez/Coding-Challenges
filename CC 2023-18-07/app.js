0d97ccbee8ad4862c5ec78d5ebe1a528ea7c5d15
alecasanchez <alecsanchez162@gmail.com>
2023-07-19 13:23:03 -0500

    changes

---
+++ CC 2023-18-07/app.js
@@ -0,0 +1,8 @@
+//old is a symbol of measurement to calculate the appropriate time to the appropriate drink. Passing peopleWithAgeDrink(old) will return a string value. 
+//people in different age brackets drink different things. We must oragnize the approptiate drink to the appropriate person. This function must also produce a number and an output of their respective drink.
+//Floors in a building with each floor containing different items and only people with floor access are allowed to the items? 
+//function peopleWithAgeDrink(age) {
+//  //1,2,3,4 or 1 2 3
+//let age = ""
+//  (age <= 14 ? toddy : 14 < age <= 18 ? coke : 18 < age <= 21 ? beer : age > 21 : whiskey);
+//};
