ae1ad64a6c3c4e2ac023a69ba00c4eda4fe9cd01
alecasanchez <alecsanchez162@gmail.com>
2023-07-22 18:37:58 -0500

    code

---
+++ CC 2023-21-2023/app.js
@@ -0,0 +1,16 @@
+//string will be a parameter constitutes a function separating and distiguishing the  first character from the last. 
+//Create a program that will accept a string, separate the string by commas and void the first and last numbers. If there are less than three characters, produce null, otherwise the program will be as such 2 3 4 
+// example of this would be a grocery list with a toy written at the beginning and the end because the beginning and the end are the most memorable and lets say we have a younger brother who writes down toys before we leave. We have to say yeet to the toys because our parents gave us a budget. 
+//create a constant 
+
+//function array(string){
+//  const array = '';
+//  if( array < 3 ){
+//    console.log('NULL')
+//  }else{ 
+//  return String.split('').map().remove([ 0, -1]);
+//  }
+//};
+//function array(arr){
+//  return arr.split(",").slice()
+//}
