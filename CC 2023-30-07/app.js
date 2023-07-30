8f35759608907667193769a8831b5afe075c8f1d
alecasanchez <alecsanchez162@gmail.com>
2023-07-30 15:14:41 -0500

    codewars

--- CC 2023-30-07/app.js
+++ CC 2023-30-07/app.js
@@ -2,6 +2,10 @@
 //in a pile of junk there will be one needle amongst all of the junk. The junk is an array that carries multiple values. after the function finds the needle, output found the needle in this position (in the array)
 //A brilliant example is like in startwars if in the trashpit, luke lost his lightsaber. In order to find the lightsaber they must sift though the water. eventually they will find the lightsaber in the water. when they do, output find lightsaber!
 //let the array be a string
-function findNeedle(haystack){
-let array = []
-let result = arr.map(item.length)
+//function findNeedle(haystack){
+//let array = []
+//let result = arr.map(item.length)
+
+//s will be the number of stairs suzuki has climbed over 20 years
+//total steps of one year * 20 years. he climbs steps depending on how many students he has. he wants to find the total number given the numbers in the array 
+const sum = [s].reduce((partialSum, a) => partialSum + a, 0)
