34c5f96a578a9b7e19b1d95b3697e30b29ea5e43
alecasanchez <alecsanchez162@gmail.com>
2023-07-30 14:58:19 -0500

    codewars

---
+++ CC 2023-30-07/app.js
@@ -0,0 +1,7 @@
+//haystack is a parameter that will contain an array with junk and one of the junks is the needle. 
+//in a pile of junk there will be one needle amongst all of the junk. The junk is an array that carries multiple values. after the function finds the needle, output found the needle in this position (in the array)
+//A brilliant example is like in startwars if in the trashpit, luke lost his lightsaber. In order to find the lightsaber they must sift though the water. eventually they will find the lightsaber in the water. when they do, output find lightsaber!
+//let the array be a string
+function findNeedle(haystack){
+let array = []
+let result = arr.map(item.length)
