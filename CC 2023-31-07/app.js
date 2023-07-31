d009f1dfa92fe633d983e4938bef6b0c3a58f8a8
alecasanchez <alecsanchez162@gmail.com>
2023-07-31 12:00:41 -0500

    solved

--- CC 2023-31-07/app.js
+++ CC 2023-31-07/app.js
@@ -1,1 +1,10 @@
-// This is a long one. 
+// This is a long one. all of the parameters are arr. each instance musto complete a differnt task. So first of all I should read documentation and then practice the exercises and then teach them to my audience. Then I come back to codewars and try again.
+const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
